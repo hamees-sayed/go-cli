f6f4ecbb6ca24251598ae9f41d8aeb0507082743
hamees-sayed <hameessayed71@gmail.com>
2022-08-15 12:44:50 +0530

    added the initial setup

--- main.go
+++ main.go
@@ -1,7 +1,11 @@
+/*
+Copyright © 2022 NAME HERE <EMAIL ADDRESS>
+
+*/
 package main
 
-import "fmt"
+import "github.com/hamees-sayed/go-cli/cmd"
 
 func main() {
-	fmt.Println("Hello world")
+	cmd.Execute()
 }
