3b78f9cb8f8a1469917ef37b7adae112b45afff8
hamees-sayed <hameessayed71@gmail.com>
2022-08-15 13:02:11 +0530

    added code command

--- cmd/root.go
+++ cmd/root.go
@@ -16,12 +16,9 @@ import (
 var rootCmd = &cobra.Command{
 	Use:   "unzo",
 	Short: "A brief description of your application",
-	Long: `A longer description that spans multiple lines and likely contains
-examples and usage of using your application. For example:
+	Long: `"unzo" is a command that unzips and open the file in vscode:
 
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
+Type "unzo --help" for help.`,
 	// Uncomment the following line if your bare application
 	// has an action associated with it:
 	// Run: func(cmd *cobra.Command, args []string) { },
